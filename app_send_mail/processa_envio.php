cb344ec9bdb5315d9348d8de768ea5d988a821bb
Yul Brynner <yulbrynner73@gmail.com>
2023-08-09 14:12:29 -0300

    Update processa_envio.php

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -57,7 +57,7 @@
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'xxyulxx@outlook.com';                     //SMTP username
+        $mail->Username   = '';                     //SMTP username
         $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                      //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
